d24bf080c44148f52fa3a58781a5dcc23d632885
José Alamos <jialamos@uc.cl>
2016-11-04 11:21:17 -0300

    Added dummy test for Semtech LoRaMAC

---
+++ tests/loramac/main.c
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) Inria Chile 2016
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     examples
+ * @{
+ *
+ * @file
+ * @brief       Test for raw IPv6 connections
+ *
+ * @author      Martine Lenders <mlenders@inf.fu-berlin.de>
+ *
+ * This test application tests the gnrc_conn_ip module. If you select protocol 58 you can also
+ * test if gnrc is able to deal with multiple subscribers to ICMPv6 (gnrc_icmpv6 and this
+ * application).
+ *
+ * @}
+ */
+
+#include <errno.h>
+#include <stdio.h>
+
+int main(void)
+{
+    puts("LoRaMAC compiled");
+    return 0;
+}
