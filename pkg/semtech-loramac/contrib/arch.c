efd8694aa55311bd7381a6fe1f9d0ca2b04027f7
José Alamos <jialamos@uc.cl>
2016-11-08 16:49:56 -0300

    finished implementation of utilities

--- pkg/semtech-loramac/contrib/arch.c
+++ pkg/semtech-loramac/contrib/arch.c
@@ -20,6 +20,16 @@ void memcpy1( uint8_t *dst, const uint8_t *src, uint16_t size )
     }
 }
 
+
+void memcpyr( uint8_t *dst, const uint8_t *src, uint16_t size )
+{
+   dst = dst + ( size - 1 );
+   while( size-- )
+   {
+       *dst-- = *src++;
+   }
+}
+
 void memset1( uint8_t *dst, uint8_t value, uint16_t size )
 {
     while( size-- )
@@ -27,3 +37,20 @@ void memset1( uint8_t *dst, uint8_t value, uint16_t size )
         *dst++ = value;
     }
 }
+
+int8_t Nibble2HexChar( uint8_t a )
+{
+    if( a < 10 )
+    {
+        return '0' + a;
+    }
+    else if( a < 16 )
+    {
+        return 'A' + ( a - 10 );
+    }
+    else
+    {
+        return '?';
+    }
+}
+
