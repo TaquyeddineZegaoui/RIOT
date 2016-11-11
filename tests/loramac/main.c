7c9563c5c5602fe0d292e4b0afe20ecde1731100
José Alamos <jialamos@uc.cl>
2016-11-11 15:26:33 -0300

    added initial functions in main

--- tests/loramac/main.c
+++ tests/loramac/main.c
@@ -24,9 +24,47 @@
 
 #include <errno.h>
 #include <stdio.h>
+#include "sx1276.h"
 
+static sx1276_t sx1276;
+void init_radio(void)
+{
+    sx1276.nss_pin = SX1276_SPI_NSS;
+    sx1276.spi = SX1276_SPI;
+
+    sx1276.dio0_pin = SX1276_DIO0;
+    sx1276.dio1_pin = SX1276_DIO1;
+    sx1276.dio2_pin = SX1276_DIO2;
+    sx1276.dio3_pin = SX1276_DIO3;
+
+    sx1276.dio4_pin = (gpio_t) NULL;
+    sx1276.dio5_pin = (gpio_t) NULL;
+    sx1276.reset_pin = (gpio_t) SX1276_RESET;
+
+    sx1276_settings_t settings;
+    settings.channel = RF_FREQUENCY;
+    settings.modem = SX1276_MODEM_LORA;
+    settings.state = SX1276_RF_IDLE;
+
+    sx1276.settings = settings;
+
+    sx1276.sx1276_event_cb = event_handler_thread;
+
+    /* Launch initialization of driver and device */
+    puts("init_radio: initializing driver...");
+    sx1276_init(&sx1276);
+
+    /* Configure the device */
+    init_configs();
+
+    /* Put chip into sleep */
+    sx1276_set_sleep(&sx1276);
+
+    puts("init_radio: sx1276 initialization done");
+}
 int main(void)
 {
+    init_radio();
     puts("LoRaMAC compiled");
     return 0;
 }
