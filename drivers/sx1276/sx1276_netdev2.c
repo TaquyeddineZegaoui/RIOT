4789f837943b160acc368889cfd2ebe4d76aa316
José Alamos <jialamos@uc.cl>
2016-11-16 19:43:57 -0300

    fixed function types

--- drivers/sx1276/sx1276_netdev2.c
+++ drivers/sx1276/sx1276_netdev2.c
@@ -17,23 +17,17 @@
  */
 
 #include "net/netdev2.h"
+#include "sx1276_netdev.h"
 #include "sx1276.h"
+#include <stddef.h>
+#include <string.h>
 
-static int _send(netdev2_t *netdev, const struct iovec *vector, unsigned count);
-static int _recv(netdev2_t *netdev, void *buf, size_t len, void *info);
+static int _send(netdev2_t *netdev, const struct iovec *vector, int count);
+static int _recv(netdev2_t *netdev, char *buf, int len, void *info);
 static int _init(netdev2_t *netdev);
 static void _isr(netdev2_t *netdev);
 static int _get(netdev2_t *netdev, netopt_t opt, void *val, size_t max_len);
 static int _set(netdev2_t *netdev, netopt_t opt, void *val, size_t len);
-
-const netdev2_driver_t sx1276_driver = {
-    .send = _send,
-    .recv = _recv,
-    .init = _init,
-    .isr = _isr,
-    .get = _get,
-    .set = _set,
-};
 
 static int _init(netdev2_t *netdev)
 {
@@ -47,47 +41,43 @@ static int _init(netdev2_t *netdev)
 
     sx1276->settings = settings;
 
-    sx1276->sx1276_event_cb = event_handler_thread;
-
     /* Launch initialization of driver and device */
     puts("init_radio: initializing driver...");
-    sx1276_init(&sx1276);
+    sx1276_init(sx1276);
 
-    /* Configure the device */
-    init_configs();
+    sx1276_lora_settings_t lora_settings;
+
+    lora_settings.bandwidth = SX1276_BW_125_KHZ;
+    lora_settings.coderate = SX1276_CR_4_5;
+    lora_settings.datarate = SX1276_SF12;
+    lora_settings.crc_on = true;
+    lora_settings.freq_hop_on = false;
+    lora_settings.hop_period = 0;
+    lora_settings.implicit_header = false;
+    lora_settings.iq_inverted = false;
+    lora_settings.low_datarate_optimize = false;
+    lora_settings.payload_len = 0;
+    lora_settings.power = 14;
+    lora_settings.preamble_len = LORA_PREAMBLE_LENGTH;
+    lora_settings.rx_continuous = true;
+    lora_settings.tx_timeout = 1000 * 1000 * 30; // 30 sec
+    lora_settings.rx_timeout = LORA_SYMBOL_TIMEOUT;
+
+    sx1276_configure_lora(sx1276, &lora_settings);
+
+    sx1276_set_channel(sx1276, 868500000);
 
     /* Put chip into sleep */
-    sx1276_set_sleep(&sx1276);
+    sx1276_set_sleep(sx1276);
 
     puts("init_radio: sx1276 initialization done");
 
-    sx1276_lora_settings_t settings;
-
-    settings.bandwidth = SX1276_BW_125_KHZ;
-    settings.coderate = SX1276_CR_4_5;
-    settings.datarate = SX1276_SF12;
-    settings.crc_on = true;
-    settings.freq_hop_on = false;
-    settings.hop_period = 0;
-    settings.implicit_header = false;
-    settings.iq_inverted = false;
-    settings.low_datarate_optimize = false;
-    settings.payload_len = 0;
-    settings.power = 14;
-    settings.preamble_len = LORA_PREAMBLE_LENGTH;
-    settings.rx_continuous = true;
-    settings.tx_timeout = 1000 * 1000 * 30; // 30 sec
-    settings.rx_timeout = LORA_SYMBOL_TIMEOUT;
-
-    sx1276_configure_lora(&sx1276, &settings);
-
-    sx1276_set_channel(&sx1276, 868500000);
     return 0;
 }
 
 static void _isr(netdev2_t *netdev)
 {
-    sx1276_t *dev = (sx1276_t *) arg;
+    sx1276_t *dev = (sx1276_t *) netdev;
 
     uint8_t irq = dev->irq;
     dev->irq = 0;
@@ -114,25 +104,25 @@ static void _isr(netdev2_t *netdev)
     }
     if(irq & SX1276_IRQ_DIO5)
     {
-        sx1276_on_dio6(dev);
+        sx1276_on_dio5(dev);
     }
 }
 
-static uint8_t get_tx_len(iovec *vector, count)
+static uint8_t get_tx_len(const struct iovec *vector, unsigned count)
 {
     uint8_t len = 0;
 
     for(int i=0;i<count;i++)
     {
-        len += vector[i]->iov_len;
+        len += vector[i].iov_len;
     }
 
     return len;
 }
 
-static int _send(netdev2_t *netdev, const struct iovec *vector, unsigned count)
+static int _send(netdev2_t *netdev, const struct iovec *vector, int count)
 {
-
+    sx1276_t *dev = (sx1276_t*) netdev;
     uint8_t size;
     switch (dev->settings.modem) {
         case SX1276_MODEM_FSK:
@@ -213,12 +203,16 @@ static int _send(netdev2_t *netdev, const struct iovec *vector, unsigned count)
     /* Put chip into transfer mode */
     sx1276_set_status(dev, SX1276_RF_TX_RUNNING);
     sx1276_set_op_mode(dev, SX1276_RF_OPMODE_TRANSMITTER);
+
+    return 0;
 }
 
-static int _recv(netdev2_t *netdev, void *buf, size_t len, void *info)
+static int _recv(netdev2_t *netdev, char *buf, int len, void *info)
 {
     /* Clear IRQ */
     volatile uint8_t irq_flags = 0;
+
+    sx1276_t *dev = (sx1276_t*) netdev;
     sx1276_reg_write(dev,  SX1276_REG_LR_IRQFLAGS, SX1276_RF_LORA_IRQFLAGS_RXDONE);
 
     irq_flags = sx1276_reg_read(dev,  SX1276_REG_LR_IRQFLAGS);
@@ -298,3 +292,13 @@ static int _set(netdev2_t *netdev, netopt_t opt, void *val, size_t len)
     //TODO
     return 0;
 }
+
+const netdev2_driver_t sx1276_driver = {
+    .send = _send,
+    .recv = _recv,
+    .init = _init,
+    .isr = _isr,
+    .get = _get,
+    .set = _set,
+};
+
