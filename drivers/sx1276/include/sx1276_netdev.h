226a9b6d6e62422bae62614c25a9e709f57f830e
José Alamos <jialamos@uc.cl>
2016-11-16 17:03:40 -0300

    added sx1276 netdev header

---
+++ drivers/sx1276/include/sx1276_netdev.h
@@ -0,0 +1,13 @@
+#ifndef SX1276_NETDEV_H_
+#define SX1276_NETDEV_H_
+
+#include "net/netdev2.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+extern const netdev2_driver_t sx1276_driver;
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif
+#endif
