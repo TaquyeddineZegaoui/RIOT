0f2cf925af37cac82f2a7090b1dfb51f07cb5459
José Alamos <jialamos@uc.cl>
2016-11-22 16:53:07 -0300

    implemented _get_state

--- drivers/sx1276/sx1276.c
+++ drivers/sx1276/sx1276.c
@@ -898,7 +898,6 @@ void sx1276_reset(sx1276_t *dev)
     xtimer_usleep(1000 * 10);
 }
 
-
 void sx1276_get_op_mode(sx1276_t *dev)
 {
     return sx1276_reg_read(dev, SX1276_REG_OPMODE) & ~SX1276_RF_OPMODE_MASK;
