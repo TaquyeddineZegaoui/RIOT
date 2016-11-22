bd1b675c24bc62e725f8d86a3b92966f5990003e
José Alamos <jialamos@uc.cl>
2016-11-22 18:34:13 -0300

    implemented some options

--- drivers/sx1276/sx1276_netdev2.c
+++ drivers/sx1276/sx1276_netdev2.c
@@ -335,12 +335,42 @@ static int _get_state(sx1276_t *dev, void *val)
     return sizeof(netopt_state_t);
 }
 
+static int _get_bw(sx1276_t *dev, void *val)
+{
+    sx1276_lora_bandwidth_t bw = dev->settings.lora.bandwith;
+
+    memcpy(val, &bw, sizeof(sx1276_lora_bandwidth_t));
+    return sizeof(sx1276_lora_bandwidth_t);
+}
+
+static int _get_sf(sx1276_t *dev, void *val)
+{
+    sx1276_lora_spreading_factor_t sf = dev->settings.lora.datarate;
+
+    memcpy(val, &sf, sizeof(sx1276_lora_spreading_factor_t));
+    return sizeof(sx1276_lora_spreading_factor_t);
+}
+
+static int _get_cr(sx1276_t *dev, void *val)
+{
+    sx1276_lora_coding_rate_t cr = dev->settings.lora.coderate;
+
+    memcpy(val, &cr, sizeof(sx1276_lora_coding_rate_t));
+    return sizeof(sx1276_lora_coding_rate_t);
+}
+
 static int _get(netdev2_t *netdev, netopt_t opt, void *val, size_t max_len)
 {
     switch(opt)
     {
         case NETOPT_STATE:
             return _get_state((sx1276_t*) netdev, val);
+        case NETOPT_LORA_BANDWIDTH:
+            return _get_bw(netdev, val);
+        case NETOPT_LORA_SPREADING_FACTOR:
+            return _get_sf(netdev, val);
+        case NETOPT_LORA_CODING_RATE:
+            return _get_cr(netdev, val);
         default:
             break;
     }
@@ -353,8 +383,17 @@ static int _set(netdev2_t *netdev, netopt_t opt, void *val, size_t len)
         case NETOPT_STATE:
             return _set_state((sx1276_t*) netdev, *((netopt_state_t*) val));
             break;
-        default:
+        case NETOPT_LORA_BANDWIDTH:
+            sx1276_configure_lora_bw((sx1276_t*) netdev, *((sx1276_lora_bandwidth_t*) val));
+            return sizeof(sx1276_lora_bandwidth_t);
+        case NETOPT_LORA_SPREADING_FACTOR:
+            sx1276_configure_lora_sf((sx1276_t*) netdev, *((sx1276_lora_spreading_factor_t*) val));
+            return sizeof(sx1276_lora_spreading_factor_t);
             break;
+        case NETOPT_LORA_CODING_RATE:
+            sx1276_configure_lora_cr((sx1276_t*) netdev, *((sx1276_lora_coding_rate_t*) val));
+            return sizeof(sx1276_lora_coding_rate_t);
+        default:
     }
     return 0;
 }
