b42446291691522d5985d2e5593c5d87a55a387f
José Alamos <jialamos@uc.cl>
2016-12-07 18:49:35 -0300

    added files

---
+++ tests/lmic/main.c
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2015 Eistec AB
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License v2.1. See the file LICENSE in the top level directory for more
+ * details.
+ */
+
+/**
+ * @ingroup tests
+ * @{
+ *
+ * @file
+ * @brief       Test for servo driver
+ *
+ * This test initializes the given servo device and moves it between
+ * 1.000 -- 2.000 ms, roughly -/+ 90 degrees from the middle position if the
+ * connected servo is a standard RC servo.
+ *
+ * @author      Joakim Nohlgård <joakim.nohlgard@eistec.se>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+
+
+int main(void)
+{
+    puts("LMIC compiled°");
+    return 0;
+}
