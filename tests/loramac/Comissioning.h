c59938c9b96e1c555c00311eaeb59f9a76041eb0
Francisco <femolina@uc.cl>
2016-12-16 17:04:33 -0300

    new dev address

--- tests/loramac/Comissioning.h
+++ tests/loramac/Comissioning.h
@@ -18,7 +18,7 @@ Maintainer: Miguel Luis and Gregory Cristian
  * When set to 1 the application uses the Over-the-Air activation procedure
  * When set to 0 the application uses the Personalization activation procedure
  */
-#define OVER_THE_AIR_ACTIVATION                     1
+#define OVER_THE_AIR_ACTIVATION                     0
 
 /*!
  * Indicates if the end-device is to be connected to a private or public network
@@ -63,7 +63,7 @@ Maintainer: Miguel Luis and Gregory Cristian
 #ifdef SAMR21_XPRO 
 #define LORAWAN_DEVICE_ADDRESS                      ( uint32_t )0x166fb58e
 #else
-#define LORAWAN_DEVICE_ADDRESS                      ( uint32_t )0x17e4c4ec
+#define LORAWAN_DEVICE_ADDRESS                      ( uint32_t )0x1677f14e
 #endif
 
 /*!
