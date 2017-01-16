fa8b0302de58e7e8b92cdf7b39d6082461fc6940
Paula <paula75>
2017-01-16 12:26:50 -0300

    main change

--- examples/hello-world/main.c
+++ examples/hello-world/main.c
@@ -27,6 +27,9 @@
 
 #define TM_YEAR_OFFSET              (1900)
 
+#if RTC_NUMOF < 1
+#error "No RTC found. See the board specific periph_conf.h."
+#endif
 
 void cb(void *arg)
 {
@@ -44,10 +47,26 @@ void cb(void *arg)
 
 int main(void)
 {
-	//lpm_arch_init();
+	lpm_arch_init();
     puts("Hello World!");
     printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
 
+    rtc_init();
+ 
+    struct tm time;
+    time.tm_year = 2011 - TM_YEAR_OFFSET; // years are counted from 1900
+    time.tm_mon  = 0; // 0 = January, 11 = December
+    time.tm_mday = 13;
+    time.tm_hour = 14;
+    time.tm_min  = 15;
+    time.tm_sec  = 15;
+
+    rtc_set_time(&time);
+
+    time.tm_sec  += 10;
+
+    rtc_set_alarm(&time, cb, 0);
+    lpm_set(LPM_POWERDOWN);
     printf("HELLO! %s\n", "Paula");
     puts("Wake"); 
 
