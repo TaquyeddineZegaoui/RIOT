01b7e2972e14860719f9f67bc9d467c2e9ffca61
José Alamos <jialamos@uc.cl>
2017-02-07 16:10:22 -0300

    refactor in Encrypt

--- sys/net/link_layer/lorawan/LoRaMacCrypto.c
+++ sys/net/link_layer/lorawan/LoRaMacCrypto.c
@@ -46,7 +46,7 @@ typedef struct  __attribute__((packed)) {
     le_uint32_t fcnt;
     uint8_t u32_pad;
     uint8_t len;
-} lorawan_mic_t;
+} lorawan_block_t;
 
 /*!
  * Contains the computed MIC field.
@@ -55,12 +55,6 @@ typedef struct  __attribute__((packed)) {
  */
 static uint8_t digest[16];
 
-/*!
- * Encryption aBlock and sBlock
- */
-static uint8_t aBlock[] = { 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-                          };
 static uint8_t sBlock[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
                             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
                           };
@@ -75,7 +69,7 @@ static cmac_context_t CmacContext;
  * Aes computation context variable
  */
 
-static cipher_t    AesContext;;
+static cipher_t    AesContext;
 
 /*!
  * \brief Computes the LoRaMAC frame MIC field  
@@ -90,7 +84,7 @@ static cipher_t    AesContext;;
  */
 uint32_t LoRaMacComputeMic( const uint8_t *buffer, uint16_t size, const uint8_t *key, uint32_t address, uint8_t dir, uint32_t sequenceCounter)
 {
-    lorawan_mic_t mic;
+    lorawan_block_t mic;
     mic.fb = 0x49;
     mic.u8_pad = 0;
     mic.dir = dir;
@@ -109,29 +103,24 @@ uint32_t LoRaMacComputeMic( const uint8_t *buffer, uint16_t size, const uint8_t 
 
 void LoRaMacPayloadEncrypt( const uint8_t *buffer, uint16_t size, const uint8_t *key, uint32_t address, uint8_t dir, uint32_t sequenceCounter, uint8_t *encBuffer )
 {
+    lorawan_block_t a_block;
     uint16_t i;
     uint8_t bufferIndex = 0;
     uint16_t ctr = 1;
 
     cipher_init(&AesContext, CIPHER_AES_128, key, KEYSIZE);
 
-    aBlock[5] = dir;
-
-    aBlock[6] = ( address ) & 0xFF;
-    aBlock[7] = ( address >> 8 ) & 0xFF;
-    aBlock[8] = ( address >> 16 ) & 0xFF;
-    aBlock[9] = ( address >> 24 ) & 0xFF;
-
-    aBlock[10] = ( sequenceCounter ) & 0xFF;
-    aBlock[11] = ( sequenceCounter >> 8 ) & 0xFF;
-    aBlock[12] = ( sequenceCounter >> 16 ) & 0xFF;
-    aBlock[13] = ( sequenceCounter >> 24 ) & 0xFF;
+    a_block.fb = 0x1;
+    a_block.u8_pad = 0;
+    a_block.dir = dir;
+    a_block.dev_addr = byteorder_btoll(byteorder_htonl(address));
+    a_block.fcnt = byteorder_btoll(byteorder_htonl(sequenceCounter));
 
     while( size >= 16 )
     {
-        aBlock[15] = ( ( ctr ) & 0xFF );
+        a_block.len = ( ( ctr ) & 0xFF );
         ctr++;
-        cipher_encrypt(&AesContext, aBlock, sBlock);
+        cipher_encrypt(&AesContext, (uint8_t*) &a_block, sBlock);
         for( i = 0; i < 16; i++ )
         {
             encBuffer[bufferIndex + i] = buffer[bufferIndex + i] ^ sBlock[i];
@@ -142,8 +131,8 @@ void LoRaMacPayloadEncrypt( const uint8_t *buffer, uint16_t size, const uint8_t 
 
     if( size > 0 )
     {
-        aBlock[15] = ( ( ctr ) & 0xFF );
-        cipher_encrypt(&AesContext, aBlock, sBlock);
+        a_block.len = ( ( ctr ) & 0xFF );
+        cipher_encrypt(&AesContext, (uint8_t*) &a_block, sBlock);
         for( i = 0; i < size; i++ )
         {
             encBuffer[bufferIndex + i] = buffer[bufferIndex + i] ^ sBlock[i];
