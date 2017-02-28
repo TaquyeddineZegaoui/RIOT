2b0cb51e4b322204009e453677f69d2fdc8dd1d1
José Alamos <jialamos@uc.cl>
2017-02-28 12:41:11 -0300

    added logic to logger

--- tests/drop_watcher/logger.py
+++ tests/drop_watcher/logger.py
@@ -1,5 +1,26 @@
 import serial
+import time
+import sys
+
+raw_input("Press Enter for start measuring\n")
+t = time.time()
+raw_input("Go! Press Enter again to stop\n")
+t = time.time()-t
+
+a = raw_input("Please enter the amount of measured water\n")
+measured = float(a)
+
+flow = measured/t*3.6
+
+raw_input("Press enter for start drop watcher measurement")
+T=20
+
+t_end = time.time()+T
 ser = serial.Serial("/dev/ttyUSB0", 115200)
-while True:
+filename = str(flow)+".log"
+print "Measured flow (l/h): "+str(flow)
+f = open("output/"+filename,'w')
+while time.time() < t_end:
     line = ser.readline().replace("\n","")
-    print line
+    f.write(line)
+f.close()
